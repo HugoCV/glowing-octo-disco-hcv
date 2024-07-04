7bbdfe16cccf7ce758068fdc4c40f80f05e46e7d
Hugo Chaves <huchav24@gmail.com>
2024-07-04 17:50:10 -0600

    Challenge Number 1 and Number 2

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -3,7 +3,38 @@
 // Print all entries, across all of the *async* sources, in chronological order.
 
 module.exports = (logSources, printer) => {
-  return new Promise((resolve, reject) => {
-    resolve(console.log("Async sort complete."));
+  return new Promise(async (resolve, reject) => {
+    const resultLogEntries = [];
+    // Function to process each logSource
+    async function processSource(source) {
+      let entry = await source.popAsync();
+      while (entry) {
+        resultLogEntries.push(entry);
+        entry = await source.popAsync();
+      }
+    }
+
+    try {
+      // Create an array of promises to process each source and collect their log entries
+      const processingPromises = logSources.map((source) =>
+        processSource(source)
+      );
+
+      // Wait for all sources to be processed and gather the results
+      await Promise.all(processingPromises);
+
+      // Sort the results by date
+      resultLogEntries.sort((a, b) => new Date(a.date) - new Date(b.date));
+
+      // Print the sorted results
+      resultLogEntries.forEach((logEntry) => printer.print(logEntry));
+
+      printer.done();
+
+      resolve(console.log("Async sort complete."));
+    } catch (error) {
+      // Reject the promise if there's an error
+      reject(error);
+    }
   });
 };
--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -3,5 +3,22 @@
 // Print all entries, across all of the sources, in chronological order.
 
 module.exports = (logSources, printer) => {
+  const mergedlogEntries = [];
+  logSources.forEach((logSource) => {
+    let logEntry = logSource.pop();
+    while (logEntry !== false) {
+      mergedlogEntries.push(logEntry);
+      logEntry = logSource.pop();
+    }
+  });
+
+  // Sort the results by date
+  mergedlogEntries.sort(
+    (logA, logB) => new Date(logA.date) - new Date(logB.date)
+  );
+
+  // Print the sorted results
+  mergedlogEntries.forEach((logEntry) => printer.print(logEntry));
+  printer.done();
   return console.log("Sync sort complete.");
 };
